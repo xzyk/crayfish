c5ef2651d57b0ea15b7fd5cca135b016894e4af6
vagrant <vagrant@homestead>
2020-07-09 15:28:02 +0800

    feat:首页-商品排序和筛选

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p class="float-left">
-            由 <a href="https://www.smhyk.top" target="_blank">xzyk</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+            由 <a href="https://www.smhyk.top" target="_blank">虾閙閙</a> ©版权所有 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
         <p class="float-right"><a href="15549070665@163.com">联系我们</a></p>
