a1df18482473873e9b121587c1900f4687fddc21
vagrant <vagrant@homestead>
2020-07-09 11:29:43 +0800

    feat:管理后台-用户管理实现

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Encore\Admin\Traits\DefaultDatetimeFormat;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable;
+    use Notifiable,DefaultDatetimeFormat;
 
     /**
      * The attributes that are mass assignable.
