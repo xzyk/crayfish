88d463eecb2753c4a9e2c635a7c95fd821aaf377
vagrant <vagrant@homestead>
2020-07-10 15:53:18 +0800

    feat:创建订单

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -8,8 +8,37 @@ class ProductSku extends Model
 {
     protected $fillable = ['title', 'description', 'price', 'stock'];
 
+    // 关联商品
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    /**
+     * 减库存
+     * @User yaokai
+     * @param $amount
+     * @return mixed
+     */
+    public function decreaseStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('减库存不可小于0');
+        }
+
+        return $this->where('id', $this->id)->where('stock', '>=', $amount)->decrement('stock', $amount);
+    }
+
+    /**
+     * 加库存
+     * @User yaokai
+     * @param $amount
+     */
+    public function addStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('加库存不可小于0');
+        }
+        $this->increment('stock', $amount);
     }
 }
