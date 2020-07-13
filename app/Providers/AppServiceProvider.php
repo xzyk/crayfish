56797f861a803c598d067242e5da4dcdc43b29f8
vagrant <vagrant@homestead>
2020-07-13 13:52:24 +0800

    feat:支付宝支付实现（好像回调key有问题）

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
         // 往服务容器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay', function () {
             $config = config('pay.alipay');
-            $config['notify_url'] = 'http://requestbin.net/r/11big0h1';
+            $config['notify_url'] = 'http://requestbin.net/r/11he23k1';
             $config['return_url'] = route('payment.alipay.return');
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
