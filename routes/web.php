8cd120deba04449a83a7c540cc88c59a32a449c6
vagrant <vagrant@homestead>
2020-07-13 15:47:28 +0800

    feat(home):订单-确认收货实现

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,7 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('orders', 'OrdersController@index')->name('orders.index');
     Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
     Route::post('orders', 'OrdersController@store')->name('orders.store');
+    Route::post('orders/{order}/received', 'OrdersController@received')->name('orders.received');// 确认收货
     // 支付方式
     Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
