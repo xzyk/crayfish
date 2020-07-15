d018451e34a9f21f3051070277326fd6ecbe6cc4
vagrant <vagrant@homestead>
2020-07-15 15:41:24 +0800

    feat(admin)：优惠券管理实现

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -26,7 +26,11 @@ Route::group([
     $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('admin.orders.handle_refund');// 拒绝退款
     // 优惠券
     $router->get('coupon_codes', 'CouponCodesController@index');
-
+    $router->post('coupon_codes', 'CouponCodesController@store');
+    $router->get('coupon_codes/create', 'CouponCodesController@create');
+    $router->get('coupon_codes/{id}/edit', 'CouponCodesController@edit');
+    $router->put('coupon_codes/{id}', 'CouponCodesController@update');
+    $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');
 
 
 
